50a6f351830eb7fc01c618f17faf034407b2af76
rburt22 <52004669+rburt22@users.noreply.github.com>
2020-04-14 15:41:53 -0400

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,3 @@
 exports.index = function(req, res, next) {
-    res.render('index', { title: 'Russell Burts WebSite Node.js Express 20200414a' });
+    res.render('index', { title: 'Russell Burts WebSite Node.js Express 20200414b' });
   }
