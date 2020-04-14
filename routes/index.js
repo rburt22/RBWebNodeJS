3b53a630f991b5a9eacc5efa2d6f705dfc7de7c0
rburt22 <52004669+rburt22@users.noreply.github.com>
2020-04-14 10:30:18 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Russell Burts WebSite Node.js Express' });
+  res.render('index', { title: 'Russell Burts WebSite Node.js Express 20200414' });
 });
 
 module.exports = router;
